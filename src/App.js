c73a5acbdf184358243211dcaf944c775d6bbd84
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-03-20 12:19:00 -0400

    router fix 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
 
 //pages
 import Homepage from './pages/Homepage/index'
@@ -16,17 +16,16 @@ class App extends Component{
   render() {
     return (
 
-      <Router>
+      <HashRouter>
         <Switch>
-        <Route exact path='/bootstrap_repo' component={Homepage}/>
+        <Route exact path='/' component={Homepage}/>
         <Route exact path='/about' component={About}/>
         <Route exact path='/contact' component={Contact}/>
         <Route exact path='/portfolio' component={Portfolio}/>
         <Route exact path='/pdf' component={Pdf}/>
         <Route exact path="/404" component={NotFound}/>
-        <Redirect to="/404"/>
         </Switch>
-      </Router>
+      </HashRouter>
 
     ) 
   }  
